92d49b234c8779b823e43639491891fd9d8f1f23
Eric Hulburd <eric@arbol.org>
2020-09-13 13:40:55 -0700

    download app links

--- web/src/routes/Home.tsx
+++ web/src/routes/Home.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Card from 'react-bootstrap/Card';
 import {VideoContainer} from './VideoContainer';
 
-const TUTORIAL_LINK = 'https://www.youtube.com/embed/hi8VVPaW0eQ';
+const TUTORIAL_LINK = 'https://www.youtube.com/embed/NIGonOKmSNo';
 
 class Home extends React.Component {
 
@@ -22,10 +22,9 @@ class Home extends React.Component {
             <p>Chord Club allows you to record audio and annotate your sounds for later reference or in order to test your musical ear. We include the following functionality:</p>
             <ul>
               <li>Record audio clips up to 5 minutes.</li>
-              <li>Add tone, quality, and extension annotations to the recordings.</li>
               <li>Upload a photo of sheet music with the recording.</li>
               <li>Tag your recordings so you can review groups of relevant recordings at once.</li>
-              <li>Test your musical ear with flashcards.</li>
+              <li>Share your recordings by tag with friends through the app.</li>
             </ul>
             <p>Two important notes about current limitations:</p>
             <ul>
@@ -33,17 +32,37 @@ class Home extends React.Component {
               <li>This is not meant for recording production quality audio.</li>
             </ul>
             <h3>Try it out!</h3>
-            <p>
-              While we're ironing out our initial launch, your feedback would be greatly appreciated.
-            </p>
-            <h4>iPhone and iPad</h4>
-            <p>
-              You can download our app onto your iPhone or iPad by installing the <a href="https://apps.apple.com/us/app/testflight/id899247664" target="_blank">Testflight app</a> and then <a target="_blank" href="https://testflight.apple.com/join/nAJqOH0v">joining our Beta testing group</a>.
-            </p>
-            <h4>Android</h4>
-            <p>
-              If you are an Android user and would like to start using ChordClub, let us know through our <a target="_blank" href="https://groups.google.com/forum/#!topic/chordclub-support/l4czcmUDd0A">Google Group</a>.
-            </p>
+            <div
+              style={{
+                display: 'flex', flexDirection: 'row',
+                flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center'
+              }}
+            >
+              <div className="download-app">
+                <a
+                  href="http://itunes.com/apps/chordclub"
+                >
+                  <span style={{ padding: '15px' }} className="download-app-img-container">
+                    <img
+                      src="/assets/Download_on_the_App_Store.svg"
+                      alt="Download on the appstore"
+                    />
+                  </span>
+                </a>
+              </div>
+              <div className="download-app">
+                <a
+                  href={'https://play.google.com/store/apps/details?id=app.chordclub&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'}
+                >
+                  <span className="download-app-img-container">
+                    <img
+                      alt={'Get it on Google Play'}
+                      src={'/assets/google-play-badge.png'}
+                    />
+                  </span>
+                </a>
+              </div>
+            </div>
             <h3>Need support? Have feedback or feature request?</h3>
             <p>Get a hold of us through our <a href="https://groups.google.com/forum/#!forum/chordclub-support" target="_blank">Google group</a> or submit your request through the form at the bottom of this page.</p>
           </Card.Body>
